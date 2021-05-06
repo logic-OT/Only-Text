b14e0810585e63be9b90d41bcdbea9b18877ab74
logic-OT <61668807+logic-OT@users.noreply.github.com>
2021-05-06 17:09:06 +0000

    Update settings.py

--- Chat_project/settings.py
+++ Chat_project/settings.py
@@ -77,7 +77,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [(".herokuapp.com", 6379)],
         },
     },
 }
